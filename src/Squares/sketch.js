8b298224087407c45656bbcb3e567fb999af0267
Zaahir Moolla <moollaza@fastmail.fm>
2021-05-20 23:41:31 -0400

    switch to cirles

--- src/Squares/sketch.js
+++ src/Squares/sketch.js
@@ -3,33 +3,28 @@ let size = window.innerWidth * 0.75;
 
 let finalSize = 4;
 let startSteps;
-let offset = 2;
+let offset = 5;
 let tileStep = (size - offset * 2) / 10;
 let startSize = tileStep;
 let directions = [-1, 0, 1];
 
 let colors = [
-  "10002b",
-  "240046",
-  "3c096c",
-  "5a189a",
-  "7b2cbf",
-  "9d4edd",
-  "c77dff",
-  "e0aaff",
+  "ffadad",
+  "ffd6a5",
+  "fdffb6",
+  "caffbf",
+  "9bf6ff",
+  "a0c4ff",
+  "bdb2ff",
+  "ffc6ff",
+  "fffffc",
 ];
-
-const palettes = {
-  a: ["233d4d", "fe7f2d", "fcca46", "a1c181", "619b8a"],
-};
-
-colors = palettes.a;
 
 function setup() {
   createCanvas(size, size);
 
   strokeWeight(2);
-  background("#" + "001427");
+  background("#" + "1f2041");
 
   noFill();
 
@@ -40,7 +35,7 @@ function setup() {
 function draw() {
   for (var x = offset; x < size - offset; x += tileStep) {
     for (var y = offset; y < size - offset; y += tileStep) {
-      startSteps = 2 + ceil(random() * 3);
+      startSteps = 2 + int(random(1, 4));
       var xDirection = directions[floor(random() * directions.length)];
       var yDirection = directions[floor(random() * directions.length)];
 
@@ -58,11 +53,13 @@ function draw() {
 }
 
 function drawSquare(x, y, width, height, xMovement, yMovement, steps) {
-  let rand = floor(random(0, colors.length));
-  let clr = colors[rand];
-  stroke(`#${clr}`);
+  stroke("#" + random(colors));
 
-  rect(x, y, width, height);
+  if (random() <= 0.25) {
+    // rect(x, y, width, height);
+  } else {
+    circle(x + width / 2, y + width / 2, width);
+  }
 
   if (steps >= 0) {
     var newSize = startSize * (steps / startSteps) + finalSize;
