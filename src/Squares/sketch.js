cef4b0b5652ffd6536f4fe1a13e6518b5ce794ec
Zaahir Moolla <moollaza@fastmail.fm>
2021-05-21 00:07:23 -0400

    messing around with opacity

--- src/Squares/sketch.js
+++ src/Squares/sketch.js
@@ -1,30 +1,23 @@
 // Vars
 let size = window.innerWidth * 0.75;
 
-let finalSize = 4;
+let cols = 11;
+let finalSize = 10;
 let startSteps;
 let offset = 5;
-let tileStep = (size - offset * 2) / 10;
+let tileStep = (size - offset * 2) / cols;
 let startSize = tileStep;
 let directions = [-1, 0, 1];
 
-let colors = [
-  "ffadad",
-  "ffd6a5",
-  "fdffb6",
-  "caffbf",
-  "9bf6ff",
-  "a0c4ff",
-  "bdb2ff",
-  "ffc6ff",
-  "fffffc",
-];
+let colors = ["ee6055", "60d394", "aaf683", "ffd97d", "ff9b85"];
 
 function setup() {
   createCanvas(size, size);
 
   strokeWeight(2);
   background("#" + "1f2041");
+
+  // blendMode(ADD);
 
   noFill();
 
@@ -35,9 +28,9 @@ function setup() {
 function draw() {
   for (var x = offset; x < size - offset; x += tileStep) {
     for (var y = offset; y < size - offset; y += tileStep) {
-      startSteps = 2 + int(random(1, 4));
-      var xDirection = directions[floor(random() * directions.length)];
-      var yDirection = directions[floor(random() * directions.length)];
+      startSteps = 2 + int(random(1, 2));
+      var xDirection = random(directions);
+      var yDirection = random(directions);
 
       drawSquare(
         x,
@@ -53,13 +46,16 @@ function draw() {
 }
 
 function drawSquare(x, y, width, height, xMovement, yMovement, steps) {
-  stroke("#" + random(colors));
+  let c = color("#" + random(colors));
+  stroke(c);
+  c.setAlpha(15);
+  fill(c);
 
-  if (random() <= 0.25) {
-    // rect(x, y, width, height);
-  } else {
-    circle(x + width / 2, y + width / 2, width);
-  }
+  // if (random() <= 0.05) {
+  // rect(x, y, width, height);
+  // } else {
+  circle(x + width / 2, y + width / 2, width);
+  // }
 
   if (steps >= 0) {
     var newSize = startSize * (steps / startSteps) + finalSize;
