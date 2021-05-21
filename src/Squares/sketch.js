9354f0a7f04bd8cd662bfcb4f02fe56eb1f9b68c
Zaahir Moolla <moollaza@fastmail.fm>
2021-05-20 22:57:06 -0400

    Working with random colours

--- src/Squares/sketch.js
+++ src/Squares/sketch.js
@@ -1,40 +1,75 @@
 // Vars
-let size, squareSize, row, col;
+let size = window.innerWidth * 0.75;
 
-let colors = {};
+let finalSize = 4;
+let startSteps;
+let offset = 2;
+let tileStep = (size - offset * 2) / 10;
+let startSize = tileStep;
+let directions = [-1, 0, 1];
+
+let colors = [
+  "10002b",
+  "240046",
+  "3c096c",
+  "5a189a",
+  "7b2cbf",
+  "9d4edd",
+  "c77dff",
+  "e0aaff",
+];
 
 const palettes = {
-  a: { bg: "03071e", from: "ffba08", to: "9d0208" },
-  b: { bg: "10002b", from: "e0aaff", to: "240046" },
-  c: { bg: "ffca3a", from: "1982c4", to: "ff595e" },
-  d: { bg: "8d0801", from: "f4d58d", to: "001427" },
-  e: { bg: "455e89", from: "5c4d7d", to: "a01a58" },
+  a: ["233d4d", "fe7f2d", "fcca46", "a1c181", "619b8a"],
 };
 
-const palette = palettes.a;
-
-const cols = 20;
-const rows = 20;
-const rotateMultiplier = 25;
-const randomDisplacement = 15;
+colors = palettes.a;
 
 function setup() {
-  size = windowWidth * 0.8;
-  squareSize = floor(size / rows);
+  createCanvas(size, size);
 
-  createCanvas(squareSize * (cols + 2), size);
+  strokeWeight(2);
+  background("#" + "001427");
 
-  background(255);
-  strokeWeight(1);
-  strokeCap(SQUARE);
-
-  rectMode(CENTER);
   noFill();
 
   // Must be last line
   noLoop();
 }
 
-function draw() {}
+function draw() {
+  for (var x = offset; x < size - offset; x += tileStep) {
+    for (var y = offset; y < size - offset; y += tileStep) {
+      startSteps = 2 + ceil(random() * 3);
+      var xDirection = directions[floor(random() * directions.length)];
+      var yDirection = directions[floor(random() * directions.length)];
 
-function drawSquare(x, y) {}
+      drawSquare(
+        x,
+        y,
+        startSize,
+        startSize,
+        xDirection,
+        yDirection,
+        startSteps - 1
+      );
+    }
+  }
+}
+
+function drawSquare(x, y, width, height, xMovement, yMovement, steps) {
+  let rand = floor(random(0, colors.length));
+  let clr = colors[rand];
+  stroke(`#${clr}`);
+
+  rect(x, y, width, height);
+
+  if (steps >= 0) {
+    var newSize = startSize * (steps / startSteps) + finalSize;
+    var newX = x + (width - newSize) / 2;
+    var newY = y + (height - newSize) / 2;
+    newX = newX - ((x - newX) / (steps + 2)) * xMovement;
+    newY = newY - ((y - newY) / (steps + 2)) * yMovement;
+    drawSquare(newX, newY, newSize, newSize, xMovement, yMovement, steps - 1);
+  }
+}
